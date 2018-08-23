fe35f0496ff0022357a1283248faed6b7c0ffd34
Eric Landheer <e.landheer@mentechmedia.nl>
2018-08-23 09:09:21 +0200

    :bug: Brainfart, remove HTML from markdown

--- resources/views/cms/emails/newsletter/new-email-subscription.blade.php
+++ resources/views/cms/emails/newsletter/new-email-subscription.blade.php
@@ -1,12 +1,11 @@
 @component('mail::message')
 
-    <p>Beste Jos,</p>
-    <br/>
-    <p>Er is een nieuwsbriefinschrijving met de volgende gegevens:</p>
-    <br/>
-    <p>Naam: {{ $emailSubscription->name }}</p>
-    <p>Email: {{ $emailSubscription->email_address }}</p>
-    <br/>
-    <p>Deze gegevens zijn ook te bekijken in het beheersysteem.</p>
-    <br/>
+    Beste Jos,
+
+    Er is een nieuwsbriefinschrijving met de volgende gegevens:
+
+    Naam: {{ $emailSubscription->name }}
+    Email: {{ $emailSubscription->email_address }}
+
+    Deze gegevens zijn ook te bekijken in het beheersysteem.
 @endcomponent
--- resources/views/cms/emails/newsletter/user-confirmation.blade.php
+++ resources/views/cms/emails/newsletter/user-confirmation.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
-    <p>Beste {{ $emailSubscription->name }},</p>
-    <br/>
-    <p>Je hebt je ingeschreven voor de nieuwsbrief van Briqchain.com:</p
-    <br/>
-    <p>Naam: {{ $emailSubscription->name }}</p>
-    <p>Email: {{ $emailSubscription->email_address }}</p>
-    <br/>
-    <p>Je kan je te alle tijden uitschrijven voor deze nieuwsbrief via de link in een nieuwsbrief.</p>
-    <br/>
-    <p>Met vriendelijke groet,</p>
-    <p>Team Briqchain</p>
+    Beste {{ $emailSubscription->name }},
+
+    Je hebt je ingeschreven voor de nieuwsbrief van Briqchain.com:
+
+    Naam: {{ $emailSubscription->name }}
+    Email: {{ $emailSubscription->email_address }}
+
+    Je kan je te alle tijden uitschrijven voor deze nieuwsbrief via de link in een nieuwsbrief.
+
+    Met vriendelijke groet,
+    Team Briqchain
 @endcomponent
