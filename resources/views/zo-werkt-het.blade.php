47525b3eb1a19b7cf340031dcffe8435c86a5826
Eric Landheer <e.landheer@mentechmedia.nl>
2018-11-21 15:14:52 +0100

    :rainbow: BRIQ-71 zo werkt het changes

--- resources/views/zo-werkt-het.blade.php
+++ resources/views/zo-werkt-het.blade.php
@@ -14,64 +14,107 @@
 		@include('partials.header', ['light' => true])
 	</div>
 	<div class="frame__row frame__row--expand">
-		<div class="fondsen">
-			<div class="fondsen__content">
-				<div class="wrapper">
-					<div class="fonds fonds-exception">
-						<div class="fonds__content">
-							<div class="fonds_omschrijving">
-								<h2 class="fonds__omschrijving-title">{!! $zoWerktHet->getStructuredText('zo_werkt_het.new_intro_title')->asHtml() !!}</h2>
-								<div class="fonds__omschrijving-paragraphs">
-									{!! $zoWerktHet->getStructuredText('zo_werkt_het.new_intro_content')->asHtml() !!}
-								</div>
-								
-								<div class="text-center mb-20">
-									<iframe id="briq_video" class="sub-intro__video" src="https://player.vimeo.com/video/249152130?color=4d31f2&title=0&byline=0&portrait=0" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-								</div>
-							</div>
+		<h1 class="title">{!! $zoWerktHet->getStructuredText('zo_werkt_het.intro_titel')->asHtml() !!}</h1>
 
-							<div class="fonds__omschrijving" style="padding-bottom: 30px;">
-								<h2 class="fonds__omschrijving-title">{!! $zoWerktHet->getStructuredText('zo_werkt_het.title')->asHtml() !!}</h2>
-								<div class="flex">
-									@foreach($zoWerktHet->steps() as $step)
-										<div class="flex-1 text-center">
-											<img class="img-responsive" style="width: 100px; height:100px;" src="{!! $step->getImage('image')->getUrl() !!}" alt="{{ $step->getText('content') }}" />
-											<h2 class="fonds__omschrijving-title">
-												{!! $step->getStructuredText('title')->asHtml() !!}
-											</h2>
-											{!! $step->getStructuredText('content')->asHtml() !!}	
-										</div>
-									@endforeach
-								</div>
+		<div class="intro" style="padding-top: 50px;">
+			<div class="wrapper wrapper--middle">
+				<div class="split-content">
+					<div class="split-content__left">
+						<div class="house-icon">
+							<div class="text-center" style="min-width: 480px;">
+								<iframe id="briq_video" class="sub-intro__video" src="https://player.vimeo.com/video/249152130?color=4d31f2&title=0&byline=0&portrait=0" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 							</div>
-							
-							<div class="fonds__omschrijving">
-								<h2 class="fonds__omschrijving-title">{!! $zoWerktHet->getStructuredText('zo_werkt_het.costs_title')->asHtml() !!}</h2>
-								<div class="fonds__omschrijving-paragraphs" style="margin-bottom: 30px;">
-                                        {!! $zoWerktHet->getStructuredText('zo_werkt_het.costs_content')->asHtml() !!}
-								</div>
-                                <h2 class="fonds__omschrijving-title">{!! $zoWerktHet->getStructuredText('zo_werkt_het.benefits_title')->asHtml() !!}</h2>
-                                <div class="fonds__omschrijving-paragraphs check-ul" style="margin-bottom: 30px;">
-                                        {!! $zoWerktHet->getStructuredText('zo_werkt_het.benefits_content')->asHtml() !!}
-                                </div>
-                                <h2 class="fonds__omschrijving-title">{!! $zoWerktHet->getStructuredText('zo_werkt_het.risks_title')->asHtml() !!}</h2>
-                                <div class="fonds__omschrijving-paragraphs check-ul">
-                                        {!! $zoWerktHet->getStructuredText('zo_werkt_het.risks_content')->asHtml() !!}
-                                </div>
+						</div>
+					</div>
+					<div class="split-content__right">
+						<h2 class="split-content__title split-content__title--purple">{!! $zoWerktHet->getStructuredText('zo_werkt_het.new_intro_title')->asHtml() !!}</h2>
+
+						{!! $zoWerktHet->getStructuredText('zo_werkt_het.new_intro_content')->asHtml() !!}
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="points">
+			<div class="points__topline">
+				<svg class="points__line-svg" viewBox="0 0 1920 100" preserveAspectRatio="none">
+					<polygon points="1920 0 1920 100 0 100 1920 0" style="fill:#ebf4fb"/>
+				</svg>
+			</div>
+			<div class="points__content">
+				<div class="wrapper wrapper--large">
+					<div class="flex">
+						@foreach($zoWerktHet->steps() as $step)
+							<div class="flex-1 text-center">
+								<img class="img-responsive" style="width: 100px; height:100px;" src="{!! $step->getImage('image')->getUrl() !!}" alt="{{ $step->getText('content') }}" />
+								<h2 class="fonds__omschrijving-title">
+									{!! $step->getStructuredText('title')->asHtml() !!}
+								</h2>
+								{!! $step->getStructuredText('content')->asHtml() !!}	
 							</div>
+						@endforeach
+					</div>
+				</div>
+			</div>
+			<div class="points__bottomline">
+				<svg class="points__line-svg" viewBox="0 0 1920 100" preserveAspectRatio="none">
+					<polygon points="0 100 0 0 1920 0 0 100" style="fill:#ebf4fb"/>
+				</svg>
+			</div>
+		</div>
+
+		<div class="blockchain">
+			<div class="wrapper wrapper--middle">
+				<div class="split-content">
+					<div class="split-content__pane split-content__left">
+						<h2 class="split-content__title split-content__title--green">{!! $zoWerktHet->getStructuredText('zo_werkt_het.costs_title')->asHtml() !!}</h2>
+						{!! $zoWerktHet->getStructuredText('zo_werkt_het.costs_content')->asHtml() !!}
+					</div>
+					<div class="split-content__pane split-content__right split-content__pane--mobile-first">
+						<div class="blockchain-icon">
+							{!! $zoWerktHet->getImage('zo_werkt_het.costs_image')->asHtml() !!}
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="fondsen__houses-left"></div>
-			<div class="fondsen__houses-right"></div>
 		</div>
-		
-		<div class="calculator" id="calculator">
-			@include('partials/calculator')
+
+		<div class="intro">
+			<div class="wrapper wrapper--middle">
+				<div class="split-content">
+					<div class="split-content__left">
+						<div class="house-icon">
+							<div class="text-center">
+								{!! $zoWerktHet->getImage('zo_werkt_het.benefits_image')->asHtml() !!}								
+							</div>
+						</div>
+					</div>
+					<div class="split-content__right check-ul">
+						<h2 class="split-content__title split-content__title--purple">{!! $zoWerktHet->getStructuredText('zo_werkt_het.benefits_title')->asHtml() !!}</h2>
+
+						{!! $zoWerktHet->getStructuredText('zo_werkt_het.benefits_content')->asHtml() !!}
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="blockchain">
+			<div class="wrapper wrapper--middle">
+				<div class="split-content">
+					<div class="split-content__pane split-content__left">
+						<h2 class="split-content__title split-content__title--green">{!! $zoWerktHet->getStructuredText('zo_werkt_het.risks_title')->asHtml() !!}</h2>
+						{!! $zoWerktHet->getStructuredText('zo_werkt_het.risks_content')->asHtml() !!}
+					</div>
+					<div class="split-content__pane split-content__right split-content__pane--mobile-first">
+						<div class="blockchain-icon">
+							{!! $zoWerktHet->getImage('zo_werkt_het.risks_image')->asHtml() !!}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="frame__row">
+	<div>
 		@include('partials/footer')
 	</div>
 </div>
