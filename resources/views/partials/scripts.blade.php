79a04223c9ca09429d8199baf85a4486e9a7b061
Eric Landheer <e.landheer@mentechmedia.nl>
2018-11-26 12:21:37 +0100

    :rainbow: BRIQ-71 increment JS and CSS version

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -16,7 +16,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/collect.js/4.0.19/collect.min.js"></script>
 
 {{--Application app.js--}}
-<script type="text/javascript" src="js/app.js?1.1.3"></script>
+<script type="text/javascript" src="js/app.js?1.1.4"></script>
 
 {{--Vimeo player api script--}}
 <script src="https://player.vimeo.com/api/player.js"></script>
