da9e8eca3de9f3c8d68aeef8b73beaad3cfb942f
Eric Landheer <e.landheer@mentechmedia.nl>
2018-11-30 12:44:43 +0100

    :art: Add styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -120,7 +120,7 @@
 
                                 <div class="point">
                                     <div class="point__icon">
-                                        {!! $point->getImage('image')->asHtml() !!}
+                                        <img src="{!! $point->getImage('image')->getUrl() !!}" style="max-width: 150px;"/>
                                     </div>
                                     {!! $point->getStructuredText('punt_title')->asHtml() !!}
                                 </div>
